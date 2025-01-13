ecd9689cc4bdff8a877896707fba7f68d3588516
Riccardo Quercetti <riccardo.quercetti.93@gmail.com>
2025-01-13 12:33:09 +0100

    chore: settings.gradle.kts reformat

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
+rootProject.name = "OOP24-roofsense"
+
 plugins {
     // foojay-resolver plugin to automatic download JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-
-rootProject.name = "OOP24-roofsense"
 
 include("chirpstack-simulator")
