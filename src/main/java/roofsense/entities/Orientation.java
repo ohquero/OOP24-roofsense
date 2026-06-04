d8cfa681e0f1fd9ffce1a5599569567c111216c7
Riccardo Quercetti <riccardo.quercetti.93@gmail.com>
2026-06-04 14:55:02 +0200

    aggiunta classe orientation

---
+++ src/main/java/roofsense/entities/Orientation.java
@@ -0,0 +1,47 @@
+package roofsense.entities;
+
+/**
+ * Represents the orientation of a measurement point according to cardinal and intercardinal directions.
+ */
+public enum Orientation {
+
+    /**
+     * North orientation.
+     */
+    N,
+
+    /**
+     * North-East orientation.
+     */
+    NE,
+
+    /**
+     * East orientation.
+     */
+    E,
+
+    /**
+     * South-East orientation.
+     */
+    SE,
+
+    /**
+     * South orientation.
+     */
+    S,
+
+    /**
+     * South-West orientation.
+     */
+    SW,
+
+    /**
+     * West orientation.
+     */
+    W,
+
+    /**
+     * North-West orientation.
+     */
+    NW
+}
